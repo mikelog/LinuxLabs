18ce9015fc9fbb6886fe2d6970b9ad1339469ec5
Mikhail Loginov <mloginov@softbcom.com>
2019-07-09 13:07:38 +0300

    Lab-11

--- lab-11/src/pam_check.sh
+++ lab-11/src/pam_check.sh
@@ -3,16 +3,10 @@ isHolyDay=$(curl  --max-time 10 --connect-timeout 5 https://isdayoff.ru/"$(date 
 dayOfWeek=$(date +%u)
 isAdmin=$(groups "$PAM_USER" | grep admin -c)
 if [[ $isAdmin -eq "1" ]];then
- echo "$(date +%Y-%m-%d) $(groups "$PAM_USER")" >> ~/pam_exec.log
- echo "$(date +%Y-%m-%d) isHolyDay=$isHolyDay dayOfWeek=$dayOfWeek isAdmin=$isAdmin" >> ~/pam_exec.log
  exit 0
 elif [[ $isHolyDay -eq "1" ]] || [[ $dayOfWeek -gt "5" ]];then
- echo "$(date +%Y-%m-%d) $(groups "$PAM_USER")" >> ~/pam_exec.log
- echo "$(date +%Y-%m-%d) isHolyDay=$isHolyDay dayOfWeek=$dayOfWeek isAdmin=$isAdmin" >> ~/pam_exec.log
  exit 1
 elif [[ $isHolyDay -eq "0" ]] || [[ $dayOfWeek -lt "6" ]];then
- echo "$(date +%Y-%m-%d) $(groups "$PAM_USER")" >> ~/pam_exec.log
- echo "$(date +%Y-%m-%d) isHolyDay=$isHolyDay dayOfWeek=$dayOfWeek isAdmin=$isAdmin" >> ~/pam_exec.log
  exit 0
 else
  exit 1
