2d3f9527f90d753db9c3af6776933d698d68e35c
Mikhail Loginov <lmi@t-sk.ru>
2019-11-13 18:26:54 +0300

    Mysql Master|Slave GTID replication

--- lab-16/README.md
+++
@@ -1,4 +0,0 @@
-## Bacula
-1. [bacula_log.txt](./bacula_log.txt) вывод команд
-2. [bacula-server](./etc-server) - конфиги сервера бакулы
-3. [bacula-client](./etc-client) - конфиг клиента
--- lab-20/files/sshd_cfg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#allow to ssh login via password
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i 's/UsePAM no/UsePAM yes/g'/etc/ssh/sshd_config
-systemctl restart sshd
